3955923a57cdee81a255193592c8a83a4f753c87
chisom okeoma <xommyb@gmail.com>
2022-03-10 15:56:24 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/**
+ * main - program startup
+ (*
+ *Description: prints the exact phrase followed by a new line)?
+ *Return: return (0) is the required function signature
+ */
+
+
+
+#include <stdio.h>
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+						return (0);
+}
