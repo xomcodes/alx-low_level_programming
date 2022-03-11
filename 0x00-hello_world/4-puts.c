51f00c1cef29b0c5c79ca688182e44c86a9336ce
chisom okeoma <xommyb@gmail.com>
2022-03-10 16:10:15 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,9 @@
  */
 
 
-
 #include <stdio.h>
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+			puts("\"Programming is like building a multilingual puzzle");
 						return (0);
 }
