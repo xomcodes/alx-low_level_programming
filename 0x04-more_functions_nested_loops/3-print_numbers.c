4af4c549a04ebf2e42fde73fc42a50e76efa4af6
xomcodes <xommyb@gmail.com>
2022-03-17 19:52:59 +0000

    prints numbers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,16 @@
-kkkkkkk
+#include "main.h"
+/**
+ *print_numbers - prints 0 - 9.
+ *
+ *Return: void.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
