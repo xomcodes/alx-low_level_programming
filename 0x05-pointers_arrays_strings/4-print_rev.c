c942856c930d2ccbfac808380a8bc98677d26d16
xomcodes <xommyb@gmail.com>
2022-03-22 22:56:47 +0000

    prints string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,23 @@
-ooooo
+#include "main.h"
+
+/**
+* print_rev - prints a string in reverse
+* @s: parameter pointer
+*
+* Return: void.
+*/
+
+void print_rev(char *s)
+{
+	int r = 0;
+
+	while (s[r])
+	{
+		r++;
+	}
+	while (r--)
+	{
+		_putchar(s[r]);
+	}
+	_putchar('\n');
+}
