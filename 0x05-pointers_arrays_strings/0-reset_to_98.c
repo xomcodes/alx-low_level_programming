ff20da3c60080a5fbffbbfb0cb7e9aba02391c5e
xomcodes <xommyb@gmail.com>
2022-03-22 22:02:16 +0000

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - reset n to 98
- * @n: input integer
- *
- * Return: nothing
- */
+* reset_to_98 - reset n to 98
+* @n: integer parameter expected upon function call
+*
+* Return: nothing
+*/
 
 void reset_to_98(int *n)
 {
-		*n = 98;
-}		
+	*n = 98;
+}
