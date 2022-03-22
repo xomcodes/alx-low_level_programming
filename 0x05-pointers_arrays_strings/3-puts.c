8e498ccc641e1f5d994088e3f04a04e19775be5a
xomcodes <xommyb@gmail.com>
2022-03-22 22:47:47 +0000

    prints string plus new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,20 @@
-kkki
+#include "main.h"
+
+/**
+* _puts - prints single line in stdout
+* @str: string parameter
+*
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
