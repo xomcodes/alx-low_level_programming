226309ffee514a5179a6c625872dca54f6b7c70e
chisom okeoma <xommyb@gmail.com>
2022-03-22 09:55:35 -0700

    _putchar.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * reset_to_98 function that takes a pointer to an int and updates
+ * reset_to_98 - reset n to 98
  * @n: input integer
  *
  * Return: nothing
  */
 
-void reset_to_98(int n)
+void reset_to_98(int 'n)
 {
-	*n = 98;
-}	
+		*n = 98;
+}
+		
+
