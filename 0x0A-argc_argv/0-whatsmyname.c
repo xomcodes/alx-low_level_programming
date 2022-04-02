40740d028038360046254b44266f3f41505d3898
xomcodes <xommyb@gmail.com>
2022-04-02 03:53:32 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,12 @@
-iiiiii
+#include <stdio.h>
+/**
+ * main - function prints its name
+ * @argc: length argument
+ * @argv: array argument
+ * Return: 0 Success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
