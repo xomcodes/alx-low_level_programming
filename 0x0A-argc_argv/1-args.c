7b2059e2fa0e42d43f38cadeb3f2ad8d998c28bb
xomcodes <xommyb@gmail.com>
2022-04-02 03:54:58 +0100

    create 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,12 @@
-iiiiiii
+#include <stdio.h>
+/**
+ * main - function prints the number of arguments passed into it
+ * @argc: length argument
+ * @argv: array argument
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
