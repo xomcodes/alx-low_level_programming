58854193ecce3708444c55a94c5b5e63ec9be3af
xomcodes <xommyb@gmail.com>
2022-04-02 03:56:09 +0100

    create 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,17 @@
-iiii
+#include <stdio.h>
+/**
+ * main - function prints all arguments its receives
+ * @argc: length argument
+ * @argv: array argument
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
