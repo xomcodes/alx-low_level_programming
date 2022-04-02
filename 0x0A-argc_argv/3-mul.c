359d010ce985b40a6b08baefea8bb03ab6975ca2
xomcodes <xommyb@gmail.com>
2022-04-02 03:58:39 +0100

    create 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,26 @@
-iiiiiiii
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function prints all arguments its receives
+ * @argc: length argument
+ * @argv: array argument
+ * Return: 0 Success otherwise 1
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result = 0;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
+}
