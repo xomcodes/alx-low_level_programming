d44b590dc1b07b5d89cca5415223158d67b550c7
xomcodes <xommyb@gmail.com>
2022-04-05 23:16:15 +0100

    update

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,19 +1,15 @@
 #include "main.h"
+
 /**
- *_isupper - checks if letters are uppercase.
- *@c: input.
+ * _isupper - checks for uppercase
+ * @c: character to checked for case
  *
- *Return: 1 if c is uppercase
- *0 otherwise.
+ * Return: 1 if uppercase, 0 if not uppercase
  */
+
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
