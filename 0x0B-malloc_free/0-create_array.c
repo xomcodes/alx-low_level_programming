bfe27187b192b29692231f9ded84eb45195549f3
xomcodes <xommyb@gmail.com>
2022-04-18 03:43:16 +0100

    initializes a string

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * create_array - create an array of chars, and initialize with specific char
- * @size: size of array
- * @c: specific char
- * Return: Returns a pointer to the array, or NULL if it fails
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
  */
-
 char *create_array(unsigned int size, char c)
 {
-	char *a;
+	char *cr;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	a = malloc(size * sizeof(*a));
-	if (a == NULL)
+	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		a[i] = c;
+		cr[i] = c;
 
-	return (a);
+	return (cr);
 }
