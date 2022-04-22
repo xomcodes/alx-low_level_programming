54050202d7e18ec282901b00a246e21eadb3d100
xomcodes <xommyb@gmail.com>
2022-04-22 16:18:57 +0100

    node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,41 @@
-IIIIIIIII
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+	size_t new_char;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	for (new_char = 0; str[new_char]; new_char++)
+		;
+
+	new_node->len = new_char;
+	new_node->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+
+	return (*head);
+}
