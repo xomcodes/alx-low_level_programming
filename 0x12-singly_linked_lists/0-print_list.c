c916130d3e99360484bf9e9d355fb7856fe8e7a4
xomcodes <xommyb@gmail.com>
2022-04-22 15:29:28 +0100

    prints element of lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,23 @@
-iiiiiiiiiiiii
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t print;
+
+	print = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		print++;
+	}
+	return (print);
+}
