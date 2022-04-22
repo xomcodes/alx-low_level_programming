c89c59938df7fffc229302161ffd1e18bd49e6e2
xomcodes <xommyb@gmail.com>
2022-04-22 22:26:19 +0100

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,19 @@
-iiiiiiiii
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *last;
+
+	while ((last = head) != NULL)
+	{
+		head = head->next;
+		free(last->str);
+		free(last);
+	}
+}
