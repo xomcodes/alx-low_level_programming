a3dbb35a711bfe5552a34bffaf2cd9c6be7754d4
xomcodes <xommyb@gmail.com>
2022-04-22 22:43:45 +0100

    you are beat

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,12 @@
-iiiiiiii
+#include <stdio.h>
+
+/**
+ * bmain - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
