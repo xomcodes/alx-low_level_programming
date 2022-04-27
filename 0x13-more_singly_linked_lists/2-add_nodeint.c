2a89be5d6f749be4a6eac79ee85079738e731d55
xomcodes <xommyb@gmail.com>
2022-04-27 18:06:47 +0100

    adds new node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,1 +1,25 @@
-iiiiiii
+#include "lists.h"
+
+/**
+ * add_nodeint - add a new node at the beginning
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *point;
+
+	point = malloc(sizeof(listint_t));
+
+	if (point == NULL)
+		return (NULL);
+
+	point->n = n;
+	point->next = *head;
+	*head = point;
+
+	return (*head);
+}
