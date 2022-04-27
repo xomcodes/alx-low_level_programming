032a0e8390d4331a99d4a3d794df02d4a2bab6e1
xomcodes <xommyb@gmail.com>
2022-04-27 18:03:13 +0100

    prints element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,21 @@
-iiiiiiiii
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t the_node = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		the_node++;
+	}
+	return (the_node);
+}
