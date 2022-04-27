d960f6e6c89caacd301892675ccf17e06dc23819
xomcodes <xommyb@gmail.com>
2022-04-27 18:58:30 +0100

    sum of data

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,22 @@
-iiiiiiii
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n) of
+ * a linked list.
+ * @head: head of a list.
+ *
+ * Return: sum of all the data (n).
+ */
+int sum_listint(listint_t *head)
+{
+	int add;
+
+	add = 0;
+	while (head != NULL)
+	{
+		add += head->n;
+		head = head->next;
+	}
+
+	return (add);
+}
