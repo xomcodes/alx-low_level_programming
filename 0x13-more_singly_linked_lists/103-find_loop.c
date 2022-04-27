e96793fe7c3c3b15cd0effb3ce41e296ef23e648
xomcodes <xommyb@gmail.com>
2022-04-27 19:14:29 +0100

    advanced

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,1 +1,42 @@
-iii
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: head of a list.
+ *
+ * Return: the address of the node where the loop starts.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *p2;
+	listint_t *old;
+
+	p2 = head;
+	old = head;
+	while (head && p2 && p2->next)
+	{
+		head = head->next;
+		p2 = p2->next->next;
+
+		if (head == p2)
+		{
+			head = old;
+			old =  p2;
+			while (1)
+			{
+				p2 = old;
+				while (p2->next != head && p2->next != old)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == head)
+					break;
+
+				head = head->next;
+			}
+			return (p2->next);
+		}
+	}
+
+	return (NULL);
+}
