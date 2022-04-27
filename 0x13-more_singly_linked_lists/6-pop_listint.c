650a730c3df6ead5c463f8b2335ab0cc7a3cdd21
xomcodes <xommyb@gmail.com>
2022-04-27 18:56:23 +0100

    deletes

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,30 @@
-iiiiiii
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
+ *
+ * Return: head node's data.
+ */
+int pop_listint(listint_t **head)
+{
+	int the_node;
+	listint_t *point;
+	listint_t *bug;
+
+	if (*head == NULL)
+		return (0);
+
+	bug = *head;
+
+	the_node = bug->n;
+
+	point = bug->next;
+
+	free(bug);
+
+	*head = point;
+
+	return (the_node);
+}
