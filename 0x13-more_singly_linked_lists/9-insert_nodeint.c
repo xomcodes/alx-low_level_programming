55221f2b3650244e80e411517ff5ccd01987f158
xomcodes <xommyb@gmail.com>
2022-04-27 18:59:42 +0100

    inserts node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,1 +1,51 @@
-iiiiiii
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int a;
+	listint_t *point;
+	listint_t *h;
+
+	h = *head;
+
+	if (idx != 0)
+	{
+		for (a = 0; a < idx - 1 && h != NULL; a++)
+		{
+			h = h->next;
+		}
+	}
+
+	if (h == NULL && idx != 0)
+		return (NULL);
+
+	point = malloc(sizeof(listint_t));
+	if (point == NULL)
+		return (NULL);
+
+	point->n = n;
+
+	if (idx == 0)
+	{
+		point->next = *head;
+		*head = point;
+	}
+	else
+	{
+		point->next = h->next;
+		h->next = point;
+	}
+
+	return (point);
+}
