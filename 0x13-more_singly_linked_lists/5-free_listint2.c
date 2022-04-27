d3bf1b19044590bcc7f4929e50dbe2591114dd85
xomcodes <xommyb@gmail.com>
2022-04-27 18:49:05 +0100

    frees list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,24 @@
-iiiiiii
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *point;
+	listint_t *bug;
+
+	if (head != NULL)
+	{
+		bug = *head;
+		while ((point = bug) != NULL)
+		{
+			bug = bug->next;
+			free(point);
+		}
+		*head = NULL;
+	}
+}
