b23778144eaebc05a2a0bab112ea1b0377d52ccb
xomcodes <xommyb@gmail.com>
2022-04-29 14:10:01 +0100

    endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,17 @@
-iiiiiii
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i;
+	char *a;
+
+	i = 1;
+	a = (char *) &i;
+
+	return ((int)*a);
+}
