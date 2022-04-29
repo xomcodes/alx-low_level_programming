bde34e2e559be22c05d4ab28c70264c32306be25
xomcodes <xommyb@gmail.com>
2022-04-29 13:09:10 +0100

    converts binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,37 @@
-iiiiii
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i;
+	int arr, base_two;
+
+	if (!b)
+		return (0);
+
+	i = 0;
+
+	for (arr = 0; b[arr] != '\0'; arr++)
+		;
+
+	for (arr--, base_two = 1; arr >= 0; arr--, base_two *= 2)
+	{
+		if (b[arr] != '0' && b[arr] != '1')
+		{
+			return (0);
+		}
+
+		if (b[arr] & 1)
+		{
+			i += base_two;
+		}
+	}
+
+	return (i);
+}
